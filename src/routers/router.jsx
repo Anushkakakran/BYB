123ec119b360ea6868dc33d62c9f9a5487aae96b
Paramjeet Work <paramjeet.s@bookyourbouncer.com>
2025-08-14 14:25:13 +0530

    Protected routing

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -8,48 +8,29 @@ import Register from "../pages/AuthPages/Register";
 import Login from "../pages/AuthPages/Login";
 import ContactForm from "../pages/ContactUs";
 import BouncerDetails from "../pages/BouncerDetails/BouncerDetails";
+import PrivateRoute from "../utils/Protected";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
+      { path: "/", element: <Home /> },
+      { path: "/book-bouncer", element: <Book /> },
       {
-        path: "/",
-        element: <Home/>
+        element: <PrivateRoute />, 
+        children: [
+          { path: "/book-bouncer/:id", element: <BouncerDetails /> },
+        ],
       },
-      {
-        path: "/book-bouncer",
-        element: <Book/>,
-      },
-      {
-        path: "/book-bouncer/:id",
-        element: <BouncerDetails/>
-      },
-      {
-        path: "/about-us",
-        element: <About/>
-      },
-      {
-        path: "/sign-up",
-        element: <Register/>
-      },
-      {
-        path: "/login",
-        element: <Login/>
-      },
-     
-      {
-        path: "/oauth-success",
-        element: <OauthSuccess/>
-      },
-      {
-        path: "/contact",
-        element: <ContactForm/>
-      },
-    ]
-  }
+      { path: "/about-us", element: <About /> },
+      { path: "/sign-up", element: <Register /> },
+      { path: "/login", element: <Login /> },
+      { path: "/oauth-success", element: <OauthSuccess /> },
+      { path: "/contact", element: <ContactForm /> },
+    ],
+  },
 ]);
 
 export default router;
